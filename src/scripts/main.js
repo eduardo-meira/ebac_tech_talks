6475b9db3986c61fe1f32b9907cad0994adbf608
eduardo-meira <dudumeira76@gmail.com>
2024-10-04 21:42:00 +0100

    envio projeto completo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('ola mundo')
+AOS.init();
+
+const dataDoEvento = new Date("Dec 31 ,3000 00:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contadorDeHoras = setInterval(function () {
+   const agora = new Date();
+   const timeStampAtual = agora.getTime();
+   
+   const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
+  const horasAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60)) / (1000 * 60));
+  const segundosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60)) / 1000)
+
+  document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+}, 1000);
